6cc1da9f55af7da15cd11d0454ee5ce336db73e7
Court Cox <82842603+coxco96@users.noreply.github.com>
2022-06-10 12:16:54 -0400

    fix legend

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiY294Y285NiIsImEiOiJja3BrY2k0ZHgwa3Y0MnZwYTl3NWs4emJ5In0.ItwJEcRmF0LwO1DkHFgpZw';
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/coxco96/cl47lbr7d003t14sb0bhuhdb3',
-    zoom: 9,
-    center: [-80.493550, 25.561938],
-    maxBounds: [[-81.7, 25], [-80, 26.802]],
-    minZoom: 9
+    style: 'mapbox://styles/coxco96/cl48g9gt1000214mokqw6mtkr', // "miamiflood_layersoff" style
+    zoom: 8.2,
+    center: [-80.793550, 25.561938],
+    // maxBounds: [
+    //     [-81.7, 25],
+    //     [-80, 26.802]
+    // ],
+    minZoom: 8.2
 });
 
 
+console.log(map.getZoom());
 // wait until map has finished loading
 
 map.on('load', () => {
@@ -22,7 +26,7 @@ map.on('load', () => {
         'type': 'raster',
         'source': 'floodrisk52',
         'layout': {
-            'visibility': 'visible'
+            'visibility': 'none'
         }
     });
 
@@ -42,17 +46,26 @@ map.on('load', () => {
 });
 
 
+
+
 // after layers have rendered and before map enters idle state, check to make sure layers were added. if not, abort.
 
 map.on('idle', () => {
+
+    let visibility22 = map.getLayoutProperty('2022', 'visibility');
+    let visibility52 = map.getLayoutProperty('2052', 'visibility');
+
+    console.log("before click. 2022: " + visibility22 + ", 2052: " + visibility52);
+
     if (!map.getLayer('2052') || !map.getLayer('2022')) {
         return;
     };
 
     // create array of layer IDs
-    const toggleableLayerIds = ['2052'];
+    // (would add more IDs to array for more buttons)
+    const toggleableLayerIds = ['2022'];
 
-    // set up corresponding toggle button for each layer
+    // set up corresponding toggle button
 
     for (const id of toggleableLayerIds) {
         // skip layers that already have a button set up
@@ -64,42 +77,40 @@ map.on('idle', () => {
         const link = document.createElement('a');
         link.id = id;
         link.href = '#';
-        link.innerHTML = "Current view: " + id;
+        link.innerHTML = "Current view: " + '2022';
         link.className = 'active';
 
-        // show or hide layer when the toggle is clicked
 
+        // show or hide layer when the toggle is clicked  
         link.onclick = function (e) {
-            const clickedLayer = this.id;
+
             e.preventDefault();
             e.stopPropagation();
 
-            const visibility = map.getLayoutProperty(
-                clickedLayer,
-                'visibility'
-            );
-
             // toggle layer visibility by changing the layout object's visibility property
-            console.log(clickedLayer);
 
-            if (visibility == 'visible' && clickedLayer == '2052') {
-                // set 2052 to hidden and 2022 to visible
-                map.setLayoutProperty('2052', 'visibility', 'none');
-                map.setLayoutProperty('2022', 'visibility', 'visible')
+            if (visibility22 == 'visible') {
+                // if 2022 is visible on click... set 2052 to visible and 2022 to none
+                map.setLayoutProperty('2022', 'visibility', 'none');
+                map.setLayoutProperty('2052', 'visibility', 'visible')
                 this.className = '';
-                link.innerHTML = "Current view: 2022"
-            } else if (visibility == 'none' && clickedLayer == '2052') {
-                // set 2052 to visible and 2022 to hidden
+                link.innerHTML = "Current view: 2052"
+                visibility22 = map.getLayoutProperty('2022', 'visibility');
+                visibility52 = map.getLayoutProperty('2052', 'visibility');
+            } else if (visibility22 == 'none') {
+                // if 2022 is not visible on click... set 2022 to visible and 2052 to hidden
                 this.className = 'active';
-                link.innerHTML = "Current view: 2052";
-                map.setLayoutProperty('2052', 'visibility', 'visible');
-                map.setLayoutProperty('2022', 'visibility', 'none')
-
+                link.innerHTML = "Current view: 2022";
+                map.setLayoutProperty('2022', 'visibility', 'visible');
+                map.setLayoutProperty('2052', 'visibility', 'none')
+                visibility52 = map.getLayoutProperty('2052', 'visibility');
+                visibility22 = map.getLayoutProperty('2022', 'visibility');
             }
+            console.log("after click. 2022: " + visibility22 + ", 2052: " + visibility52);
 
         };
+
         const layers = document.getElementById('menu');
         layers.appendChild(link);
     }
 });
-
